a8bd9c34b7c3b0a4773b48a7d561b9cf7221a5d9
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2021-05-20 23:33:01 +0200

    fixed men and women button for the shop

--- js/shop.js
+++ js/shop.js
@@ -1,31 +1,58 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    /**
+     * cleans the shop interface
+     */
     function switchToMen() {
-        let img = document.getElementsByClassName("women");
-        const menShirt = ["/img/shop/white-men_tshirt_1.png", "/img/shop/white-men_tshirt_2.png"];
+        const menShirt = ["/img/shop/white-men_tshirt_1.png", "/img/shop/white-men_tshirt_2.png", "/img/shop/black_pullover_1.png", "/img/shop/black_pullover_2.png"];
+        
+        document.getElementById("start").remove();
+        document.getElementById("women_btn").remove();
+        document.getElementById("men_btn").remove();
 
-        for (let i = 0; i < menShirt.length; i++) {
-            img[i].src = menShirt[i];
-            img[i].className = "men";
-            img[i].alt = "men tshirt";
-            
-        }
+        createShop(menShirt, "men");
     }
 
+    /**
+     * cleans the shop interface
+     */
     function switchToWomen() {
-        var img = document.getElementsByClassName("men");
-        const womenShirt = ["/img/shop/white-women_tshirt_1.png", "/img/shop/white-women_shirt_2.png"];
-        let i = 0;
+        const womenShirt = ["/img/shop/white-women_tshirt_1.png", "/img/shop/white-women_tshirt_2.png", "/img/shop/black_pullover_1.png", "/img/shop/black_pullover_2.png"];
 
-        for (i = 0; i < womenShirt.length; i++) {
-            img[i].src = womenShirt[i];
-            img[i].className = "women";
-            img[i].alt = "women tshirt";
+        document.getElementById("start").remove();
+        document.getElementById("women_btn").remove();
+        document.getElementById("men_btn").remove();
+
+        createShop(womenShirt, "women");
+    }
+
+    /**
+     * displays the shop based on the array of shirts
+     * @param {array of tshirt filled with men or women shirts} list_of_shirts
+     * @param {string of which gender is to be visited} gender
+     */
+    function createShop(list_of_shirts, gender) {
+        let main = document.getElementById("mainShopScreen");
+        let table = document.createElement("table");
+        let tableR = document.createElement("tr");
+        let tableD = document.createElement("td");
+
+        // Creates for every T-Shirt an image and displays it
+        for(let i = 0; i < list_of_shirts.length; i++) {
+            let img = document.createElement("img");
+            let tmp = tableD.appendChild(img);
+            tmp.src = list_of_shirts[i];
+            tmp.className = gender;
         }
+
+        tableR.appendChild(tableD);
+        table.appendChild(tableR);
+
+        main.appendChild(table);
     }
 
     let changeToMen = document.getElementById("women_btn");
     let changeToWomen = document.getElementById("men_btn");
-    changeToMen.addEventListener("click",switchToWomen);
+    changeToMen.addEventListener("click", switchToWomen);
     changeToWomen.addEventListener("click", switchToMen);
 });
