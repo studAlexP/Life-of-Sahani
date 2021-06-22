ddf71c6d2845098124aa3e1a97bb5b17f9f1a257
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2021-06-22 12:16:05 +0200

    fixed hamburger menu

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,15 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    const hamurger = document.getElementById("hamburger");
-    const navUL = document.getElementById("nav-ul");
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 
-    hamurger.addEventListener("click", () => {
-        console.log("R");
-    })
-});
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
