6e8d2e98b9e6193b702630149826a0e8cc617c8c
Sahani <sahani070797@gmail.com>
2021-06-23 19:17:08 +0200

    Basic session management created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const session = require('express-session')
 
 app.use(cors());
 app.options('*', cors())
@@ -14,16 +15,50 @@ const PORT = process.env.PORT ||3000;
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*')
     next();
 });
 app.use((req, res, next) => {
     res.setHeader("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
 });
+
+app.use(session({
+    secret: '0800fc577294c34e0b28ad2839435945',
+    saveUninitialized: true,
+    resave: true,
+    cookie: { sameSite: 'none' },
+}))
 
 const shirtRoutes = require('./api/routes/ShirtRoutes');
 
 app.use('/api/shirts', shirtRoutes);
+
+app.post('/auth', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    if (username && password) {
+        if (username == 'Test' && password == 'Test') {
+            req.session.loggedin = true;
+            req.session.username = username;
+            res.redirect('http://localhost:5500/html/feedback.html');
+        } else {
+            res.send("Username or Password incorrect");
+        }
+        res.end();
+    }
+    res.end();
+})
+
+app.get('/logout', (req, res) => {
+    if (req.session.loggedin) {
+        req.session.destroy();
+        res.redirect('http://localhost:5500/html/login.html');
+
+    }
+    res.end();
+})
 
 app.listen(PORT, (error) => {
     if (error) {
